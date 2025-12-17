40b6b4ffc9959d9566e45b492244f6a2a8f6b7d0
Gonzalo Aliaga <gonz.aliaga@duocuc.cl>
2025-12-16 23:03:05 -0300

    fix import lombok

--- src/main/java/com/example/miapp/model/Orden.java
+++ src/main/java/com/example/miapp/model/Orden.java
@@ -4,6 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 import java.util.List;
+import lombok.Data;
 
 @Data
 @Document(collection = "ordenes")
